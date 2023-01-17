26a07fc92e18654bdb2e9e72a489acf914802a0c
AndriiPets <ulmalaut@DESKTOP-3RNKDCT.localdomain>
2023-01-17 22:32:28 +0200

    i hope final

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth"
-import { OAuthConfig } from "next-auth/providers";
 import GoogleProvider, { GoogleProfile } from "next-auth/providers/google"
 import { Session } from "next-auth"
 export const authOptions = {
